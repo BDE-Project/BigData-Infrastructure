663b2162a383100e59d2bf465da4770a50c40737
Onelam <onelamagubeni@gmail.com>
2024-11-10 10:24:20 +0200

    test workflow

--- Scripts/reddit_kinesis_1.py
+++ Scripts/reddit_kinesis_1.py
@@ -65,16 +65,16 @@ def stream_to_kinesis(submission):
     )
     print(f"Sent data to Kinesis: {response}")
 
-# Step 5: Stream new submissions from subreddit for 2 minutes
+# Step 5: Stream new submissions from subreddit for 2 hours
 start_time = datetime.now()
-max_duration = timedelta(minutes=2)  # Run the script for 2 minutes
+max_duration = timedelta(hours=1)  # Run the script for 2 hours
 
 for submission in subreddit.stream.submissions():
     stream_to_kinesis(submission)
     
-    # Break the loop if 2 minutes have passed
+    # Break the loop if 2 hours have passed
     if datetime.now() - start_time > max_duration:
-        print("Stopping script after running for 2 minutes.")
+        print("Stopping script after running for 1 hours.")
         break
 
     time.sleep(1)  # Adjust the delay if needed
